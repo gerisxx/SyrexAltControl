131ee8fa7d6c6acb61fe5436c96ce6ae7045ee4d
gerisxx <112516922+gerisxx@users.noreply.github.com>
2023-04-30 23:01:37 -0400

    resources

---
+++ codes.lua
@@ -0,0 +1,26 @@
+-- THIS WILL PROBABLY NOT BE UPDATED NO MORE
+
+local Codes = {
+  "2BVISTS",
+  "1MLIKES",
+  "@DAHOOD",
+  "militarybase",
+  "LunarNewYear",
+  "MLK",
+  "HappyNewYear2023",
+  "RifleWinter2022",
+  "#HAPPYHALLOWEEN!",
+  "BACK2SCHOOL",
+  "ACCOMPLISHMENT",
+  "AUGUST2022!",
+  "#FREED",
+  "FIREWORKS",
+  "DHSUPRISE!",
+  "2022JUNE",
+  "easterdahood",
+  "Stars",
+  "STARS",
+  "DHUpdate",
+}
+
+return Codes
